94024b81818041bcfb6cb83f1d2355ef0f2061cd
sistemasdesarrollo@delgadotravelusa.com <sistemasdesarrollo@delgadotravelusa.com>
2019-02-06 17:38:07 -0500

    (Add): calendar in Branch component

--- src/components/Branch.js
+++ src/components/Branch.js
@@ -1,16 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { DayPicker as Calendar } from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 
 export default class Branch extends Component {
     constructor(props) {
         super(props)
         this.state = {
             title: "BHDLeon",
-            minutes: 10
-        }  
+            minutes: 10,
+            date: new Date()
+        }
+
+        this.handleDate = this.handleDate.bind(this)
     }
 
-    render(){
-        return(
+    handleDate(date) {
+        console.log("DATE", date)
+        this.setState({date})
+    }
+
+    render() {
+        return (
             <div id="branch">
                 <div id="branch-title">
                     <h1>{this.state.title}</h1>
@@ -18,7 +28,7 @@ export default class Branch extends Component {
                 <div id="branch-content">
                     <div id="branch-content-server">
                         <h2>The server is being checked every {this.state.minutes}</h2>
-                        <div>
+                        <div id="branch-content-server-button">
                             <button>Change time</button>
                             <select>
                                 <option>5</option>
@@ -31,7 +41,10 @@ export default class Branch extends Component {
                     </div>
                     <div id="branch-content-calendar">
                         <h2>Choose Date</h2>
-                        <h1>Calendar</h1>
+                        <Calendar
+                            onDayClick={this.handleDate}
+                            selectedDays={this.state.date}
+                        />
                     </div>
                 </div>
                 <button id="branch-button">Send All Transactions</button>
