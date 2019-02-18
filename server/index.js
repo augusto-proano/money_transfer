70e65457792b4defa428e5fe596696a64c2863c0
augusto-proano <tito4gsus@gmail.com>
2019-02-18 10:34:39 -0500

    (Add): chalk to dependecies

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const morgan = require('morgan')
 const express = require('express')
 const bodyParser = require('body-parser')
 const compression = require('compression')
+const chalk = require('chalk')
 
 const app = express()
 module.exports = app
@@ -26,5 +27,5 @@ if (process.env.NODE_ENV === 'production')
   app.use(express.static(path.join(__dirname, '..', 'public')))
 
 app.listen(app.get('port'), () => {
-  console.log(`Server is listening on port ${app.get('port')}`)
+  console.log(`Server is listening on port ${chalk.green(app.get('port'))}`)
 })
--- server/sftpServer.js
+++ server/sftpServer.js
@@ -1,25 +1,28 @@
 const Client = require('ssh2-sftp-client')
 const sftp = new Client()
+const chalk = require('chalk')
 
 //Connection to SFTP server
-const sftpServer = sftp.connect({
-  host: 'sftp.bhdleon.com.do',
-  port: 22,
-  username: 'sftp_dtc',
-  password: 'mae!Er1m-]2.',
-  algorithms: {
-    cipher: [
-      'aes128-ctr',
-      'aes192-ctr',
-      'aes256-ctr',
-      'aes128-gcm',
-      'aes128-gcm@openssh.com',
-      'aes256-gcm',
-      'aes256-gcm@openssh.com',
-      'aes256-cbc'
-    ]
-  }
-})
+const sftpServer = sftp
+  .connect({
+    host: 'sftp.bhdleon.com.do',
+    port: 22,
+    username: 'sftp_dtc',
+    password: 'mae!Er1m-]2.',
+    algorithms: {
+      cipher: [
+        'aes128-ctr',
+        'aes192-ctr',
+        'aes256-ctr',
+        'aes128-gcm',
+        'aes128-gcm@openssh.com',
+        'aes256-gcm',
+        'aes256-gcm@openssh.com',
+        'aes256-cbc'
+      ]
+    }
+  })
+  .catch(err => console.log(chalk.magenta('SFTP SERVER: '), err))
 
 module.exports = {
   sftpServer,
